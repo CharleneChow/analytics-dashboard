b3dbb6fc861479e813782d7b3437410812d03a7b
CharleneChow <zhouxiaolinxxxx@163.com>
2024-08-15 03:07:46 +0800

    deploy

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -1,16 +1,16 @@
 // api design use class is better
 import { redis } from "@/lib/redis";
+import { getDate } from "@/utils/index";
+import { parse } from "date-fns";
+
+type TrackOptions = {
+  persist?: boolean;
+};
 
 type AnalyticsArgs = {
   retention?: number;
 };
 export class Analytics {
-  static track(
-    arg0: string,
-    arg1: { page: string; country: string | undefined }
-  ) {
-    throw new Error("Method not implemented.");
-  }
   private retention: number = 60 * 60 * 24 * 7;
   // default internal expired time: 1 week
 
@@ -19,10 +19,50 @@ export class Analytics {
       this.retention = opts.retention;
     }
   }
-  async track(namespace: string, event: object = {}) {
-    const key = `analytics::${namespace}`;
+
+  async retrieveDays(namespace: string, nDays: number) {
+    type AnalyticsPromise = ReturnType<typeof analytics.retrieve>;
+    const promises: AnalyticsPromise[] = [];
+    for (let i = 0; i < nDays; i++) {
+      const formattedDate = getDate(i);
+      const promise = analytics.retrieve(namespace, formattedDate);
+      promises.push(promise);
+    }
+
+    const fetched = await Promise.all(promises);
+    const data = fetched.sort((a, b) => {
+      if (
+        parse(a.date, "dd/MM/yyyy", new Date()) >
+        parse(b.date, "dd/MM/yyyy", new Date())
+      ) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+    return data;
+  }
+  async retrieve(namespace: string, date: string) {
+    const res = await redis.hgetall<Record<string, string>>(
+      `analytics::${namespace}::${date}`
+    );
+    return {
+      date,
+      events: Object.entries(res ?? []).map(([key, value]) => ({
+        [key]: Number(value),
+      })),
+    };
+  }
+  async track(namespace: string, event: object = {}, opts?: TrackOptions) {
+    let key = `analytics::${namespace}`;
+    if (!opts?.persist) {
+      key += `::${getDate()}`;
+    }
     // db call to persist this event
     await redis.hincrby(key, JSON.stringify(event), 1);
+    if (!opts?.persist) {
+      await redis.expire(key, this.retention);
+    }
   }
 }
 
